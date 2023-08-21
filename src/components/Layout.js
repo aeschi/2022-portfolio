a537060e69604c244d5b04494fff5ab7c332a459
aeschi <anna.eschenbacher@icloud.com>
2023-08-21 14:42:23 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -42,7 +42,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta property="og:image" content={`${withPrefix("/")}img/og-image.jpg`} />
+        <meta property="og:image" content={`${withPrefix("/")}img/remnants_i_2.jpeg`} />
       </Helmet>
       {/* <Navbar /> */}
       <div>{children}</div>
