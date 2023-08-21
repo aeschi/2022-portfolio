8bce575acc6d455010ba46350b7ee55bf976be4d
aeschi <anna.eschenbacher@icloud.com>
2023-08-21 14:35:08 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: "Anna Eschenbacher",
-    description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+    description: "Have a look at my previous work",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
